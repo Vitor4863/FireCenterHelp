87b937bae055d0a06826656a725f594d701337a6
NicolasPaltri <nicolaspaltri@gmail.com>
2023-01-23 08:49:09 -0300

    Att perfil

--- Fire/consulta/usuario.php
+++ Fire/consulta/usuario.php
@@ -17,7 +17,7 @@
         integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
         crossorigin="anonymous"/>
 
-    <link rel="stylesheet" href="user.css">
+    <link rel="stylesheet" href="user2.css">
 
     
 </head>
@@ -40,7 +40,7 @@
                     
                     <li class="list-item">
                        <i class="list-item-icon fas fa-book"></i>
-                        <a href="../visao/sobre.php"><span class="list-item-text">Sobre</span></a>
+                        <a href="../visao/sobre.php"><span class="list-item-text">Help</span></a>
                     </li>
                     <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@600&display=swap" rel="stylesheet">
         <script src="https://unpkg.com/phosphor-icons"></script>
@@ -57,11 +57,16 @@
     <section id="main_user">
       
         <div class="info">
-
+            <div class="container_icons">
+                    <a href="" id="edit"><i class='bx bx-edit icon '></i></a>
+               
+                </div>
             <h2>Perfil</h2>
 
+           
+
             <div class="container_info">
-     
+    
 
                 <div class="nome" >
                     
@@ -69,7 +74,7 @@
                     <?php
 					echo $_SESSION['nome'];
 					?> 
-                    
+                
                 </div>
 
                 <div class="underline"></div>
@@ -98,6 +103,9 @@
 					echo $_SESSION['senha'];
 					?> 
                 </div>
+
+                <div class="underline"></div>
+
                 <div class="data">
 
                     <span><strong>Data de nascimento:</strong> </span>
@@ -105,20 +113,29 @@
 					echo $_SESSION['dataNascimento'];
 					?> 
                 </div>
+
+                <div class="underline"></div>
+
                 <div class="telefone">
 
-                    <span><strong>Telefone</strong> </span>
+                    <span><strong>Telefone:</strong> </span>
                     <?php
 					echo $_SESSION['telefone'];
 					?> 
                 </div>
+
+                <div class="underline"></div>
+
                 <div class="cpf">
 
-                    <span><strong>CPF</strong> </span>
+                    <span><strong>CPF:</strong> </span>
                     <?php
 					echo  $_SESSION['cpf'];
 					?> 
                 </div>
+
+                <div class="underline"></div>
+
                  <div class="cep">
 
                     <span><strong>CEP:</strong> </span>
@@ -126,6 +143,9 @@
 					echo $_SESSION['cep'];
 					?> 
                 </div>
+
+                <div class="underline"></div>
+
                 <div class="nSegu">
 
                     <span><strong>Numero de segurança:</strong> </span>
@@ -133,13 +153,19 @@
 					echo  $_SESSION['numeroSeguranca'];
 					?> 
                 </div>
+
+                <div class="underline"></div>
+
                 <div class="alergia">
 
-                    <span><strong>Alergia</strong> </span>
+                    <span><strong>Alergia:</strong> </span>
                     <?php
 					echo $_SESSION['alergia'];
 					?> 
                 </div>
+
+                <div class="underline"></div>
+
                 <div class="tipoS">
 
                     <span><strong>Tipo Sanguineo:</strong> </span>
@@ -147,6 +173,18 @@
 					echo $_SESSION['sangue'];
 					?> 
                 </div>
+
+                <div class="underline"></div>
+
+                <div class="pcd"> 
+                    <span><strong>PCD:</strong></span>
+                    <?php 
+					echo $_SESSION['pcd'];
+					?> 
+                </div>
+
+                <div class="underline"></div>
+
                 <div class="defi">
 
                     <span><strong>Deficiencia:</strong> </span>
@@ -157,10 +195,7 @@
 
                 <div class="underline"></div>
 
-                <div class="container_icons">
-                    <a href="" id="edit"><i class='bx bx-edit icon '></i></a>
-               
-                </div>
+                
 
            
             </div>
@@ -174,37 +209,132 @@
 
                 <div class="input-data">
 
-                    <input type="text" value="" name="personName" id="personName" autocomplete="off" required>
-                    <div class="underline"></div>
-
-                    <label>Name</label>
-
+                <div class="nome" >
+                    
+                    <span><strong>Nome:</strong> </span>
+                    <?php
+					echo $_SESSION['nome'];
+					?> 
+                
                 </div>
 
-                <div class="input-data">
+                <div class="underline"></div>
 
-                    <input type="text" value="" name="userName" id="userName" autocomplete="off" required>
-                    <div class="underline"></div>
-
-                    <label>Username</label>
-
+                <div class="usuario"> 
+                    <span><strong>Usuario:</strong></span>
+                    <?php 
+					echo $_SESSION['usuario'];
+					?> 
                 </div>
-                <div class="input-data">
 
-                    <input type="password" value="" name="userPassword" id="userPassword" autocomplete="off" required>
-                    <div class="underline"></div>
+                <div class="underline"></div>
 
-                    <label>Password</label>
-
+                <div class="email">
+                    <span><strong>Email:</strong> </span>
+                    <?php
+					echo $_SESSION['email'];
+					?> 
                 </div>
-                <div class="input-data">
 
-                    <input type="text" value="" name="cpf" id="cpf" autocomplete="off" required>
-                    <div class="underline"></div>
+                <div class="underline"></div>
 
-                    <label>CPF</label>
-
+                <div class="senha">
+                    <span><strong>Senha:</strong></span>
+                    <?php
+					echo $_SESSION['senha'];
+					?> 
                 </div>
+
+                <div class="underline"></div>
+
+                <div class="data">
+
+                    <span><strong>Data de nascimento:</strong> </span>
+                    <?php
+					echo $_SESSION['dataNascimento'];
+					?> 
+                </div>
+
+                <div class="underline"></div>
+
+                <div class="telefone">
+
+                    <span><strong>Telefone:</strong> </span>
+                    <?php
+					echo $_SESSION['telefone'];
+					?> 
+                </div>
+
+                <div class="underline"></div>
+
+                <div class="cpf">
+
+                    <span><strong>CPF:</strong> </span>
+                    <?php
+					echo  $_SESSION['cpf'];
+					?> 
+                </div>
+
+                <div class="underline"></div>
+
+                 <div class="cep">
+
+                    <span><strong>CEP:</strong> </span>
+                    <?php
+					echo $_SESSION['cep'];
+					?> 
+                </div>
+
+                <div class="underline"></div>
+
+                <div class="nSegu">
+
+                    <span><strong>Numero de segurança:</strong> </span>
+                    <?php
+					echo  $_SESSION['numeroSeguranca'];
+					?> 
+                </div>
+
+                <div class="underline"></div>
+
+                <div class="alergia">
+
+                    <span><strong>Alergia:</strong> </span>
+                    <?php
+					echo $_SESSION['alergia'];
+					?> 
+                </div>
+
+                <div class="underline"></div>
+
+                <div class="tipoS">
+
+                    <span><strong>Tipo Sanguineo:</strong> </span>
+                    <?php
+					echo $_SESSION['sangue'];
+					?> 
+                </div>
+
+                <div class="underline"></div>
+
+                <div class="pcd"> 
+                    <span><strong>PCD:</strong></span>
+                    <?php 
+					echo $_SESSION['pcd'];
+					?> 
+                </div>
+
+                <div class="underline"></div>
+
+                <div class="defi">
+
+                    <span><strong>Deficiencia:</strong> </span>
+                    <?php
+					echo $_SESSION['deficiencia'];
+					?> 
+                </div>
+
+                <div class="underline"></div>
 
                 <button class="btn">Modificar</button>
 
