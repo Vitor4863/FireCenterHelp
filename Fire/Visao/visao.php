6b8d8a22873b6820d060d3f1ddbbe0b3f05bf9a8
Marcus <mvsc1604@gmail.com>
2023-01-25 09:11:50 -0300

    correçao de alguns arquivos

--- Fire/Visao/visao.php
+++ Fire/Visao/visao.php
@@ -8,7 +8,7 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style1.css">
+    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
         integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
         crossorigin="anonymous"/>
@@ -72,11 +72,11 @@ session_start();
             </div>
             <a href="#modal_1" class="btn2">Emergêcia</a>
                
-            <div class="sidebar-bottom">
+            <!--<div class="sidebar-bottom">
                 <div class="color-box dark"></div>
                 <div class="color-box night"></div>
                 <div class="color-box light"></div>
-            </div>
+            </div>-->
         </div>
 
          <!--pop-up-->
