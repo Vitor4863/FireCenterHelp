a4f23b95ca85290c12e08a65ec8ca9a57d3dd764
Marcus <mvsc1604@gmail.com>
2023-01-27 19:02:27 -0300

    att modal

--- Fire/Cadastro/main.js
+++ Fire/Cadastro/main.js
@@ -1,26 +1,62 @@
-const input = document.querySelector('input')
+function is_cpf (c) {
 
-input.addEventListener('keypress', () => {
-    let inputLength = input.value.length
-
-    // MAX LENGHT 14  CPF
-    if (inputLength == 3 || inputLength == 7) {
-        input.value += '.'
-    }else if (inputLength == 11) {
-        input.value += '-'
+    if((c = c.replace(/[^\d]/g,"")).length != 11)
+      return false
+    
+    if (c == "00000000000")
+      return false;
+    
+    var r;
+    var s = 0;
+    
+    for (i=1; i<=9; i++)
+      s = s + parseInt(c[i-1]) * (11 - i);
+    
+    r = (s * 10) % 11;
+    
+    if ((r == 10) || (r == 11))
+      r = 0;
+    
+    if (r != parseInt(c[9]))
+      return false;
+    
+    s = 0;
+    
+    for (i = 1; i <= 10; i++)
+      s = s + parseInt(c[i-1]) * (12 - i);
+    
+    r = (s * 10) % 11;
+    
+    if ((r == 10) || (r == 11))
+      r = 0;
+    
+    if (r != parseInt(c[10]))
+      return false;
+    
+    return true;
     }
-
-
-    // MAX LENGHT 12  RG
-    // if (inputLength == 2 || inputLength == 6) {
-    //     input.value += '.'
-    // }else if (inputLength == 10) {
-    //     input.value += '-'
-    // }
-
-    // // MAX LENGHT 19 CARTÃO
-    // if (inputLength == 4 || inputLength == 9 || inputLength == 14) {
-    //     input.value += ' '
-    // }
-
-})
+    
+    
+    function fMasc(objeto,mascara) {
+    obj=objeto
+    masc=mascara
+    setTimeout("fMascEx()",1)
+    }
+    
+    function fMascEx() {
+    obj.value=masc(obj.value)
+    }
+    
+    function mCPF(cpf){
+    cpf=cpf.replace(/\D/g,"")
+    cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
+    cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
+    cpf=cpf.replace(/(\d{3})(\d{1,2})$/,"$1-$2")
+    return cpf
+    }
+    
+    cpfCheck = function (el) {
+      document.getElementById('cpfResponse').innerHTML = is_cpf(el.value)? '<span style="color:green">válido</span>' : '<span style="color:red">inválido</span>';
+      if(el.value=='') document.getElementById('cpfResponse').innerHTML = '';
+      /*--------------------------------------------------------------------------------------------------------------*/
+    }
